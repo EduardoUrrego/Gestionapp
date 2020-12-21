429977909968282e7a22b8e18cc47cd923ef6a8f
Carlosavila3 <72456283+Carlosavila3@users.noreply.github.com>
2020-12-21 01:40:08 -0500

    home actualizado con imagenes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,19 +5,19 @@
         </div>
         <div class="mod__content">
             <div class="opciones-inicio">
-                <router-link to="/usuario">
                 <div class="opcion-inicio">
                     <span class="opcion-titulo">Usuarios</span>
+                <router-link to="/usuario">
                     <div class="opcion-imagen imgusuario"></div>
-                </div>
                 </router-link>
+                </div>
                 
-                <router-link to="/cliente">
                 <div class="opcion-inicio">
                     <span class="opcion-titulo">Clientes</span>
+                <router-link to="/cliente">
                     <div class="opcion-imagen imgcliente"></div>
-                </div>
                 </router-link>
+                </div>
 
             </div>
 
